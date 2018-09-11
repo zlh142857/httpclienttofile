50cb469fa425990d8cd1119ffe00bb55c4c651f0
zlh142857 <1056846491@qq.com>
2018-09-11 16:45:10 +0800

    2018/9/11 张立恒 在线预览

---
+++ src/main/java/com/hx/controller/ShowFileController.java
@@ -0,0 +1,55 @@
+package com.hx.controller;/*
+ *//*
+ *@作者:张立恒
+ *@时间:2018/9/11 16:12
+ *@功能:在线预览文件
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+public class ShowFileController {
+    /**
+     *
+     * 功能描述: 点击文件名称,在线预览
+     *
+     * @param:
+     * @return:
+     * @auther: 张立恒
+     * @date: 2018/9/11 16:13
+     */
+    @RequestMapping(value="/showImage")
+    public void showImageByType(String filename,HttpServletResponse response) throws Exception{
+        InputStream inputStream = null;
+        OutputStream writer = null;
+        try {
+            /*/usr/uploadImage/*/
+            inputStream = new FileInputStream(new File("D:\\aaaaaa\\"+filename));
+            writer = response.getOutputStream();
+
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(buf)) != -1) {
+                writer.write(buf, 0, len); //写
+            }
+            inputStream.close();
+        } catch (Exception e) {
+            System.out.println("看这里+++++++++++++++++++出错了+++++++++++++++++++++++++++");
+        } finally{
+            try {
+                if(inputStream != null){
+                    inputStream.close();
+                }
+                if(writer != null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                System.out.println("看这里finally+++出错了++++inputStream和OutputStream都是空的");
+            }
+        }
+    }
+}
